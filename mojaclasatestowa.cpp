2823d6afc25ab03573210cebb36585e3148d8a10
arturkaminski <muir.e@students.rutc.ac.uk>
2021-04-09 16:36:07 +0200

    Add files via upload

---
+++ mojaclasatestowa.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+class User{
+	private:
+		std::string name = "Artur";
+		int age = 5;
+	public:
+		void userAsk();
+		void setAge(int newAge){
+			this->age = newAge;
+		}
+		void getName(){
+		std::cout << this->name << ": " << this->age << std::endl;
+                }
+	};
+	User::userAsk(){
+		std::cout << "What is his age" << std::endl;
+	}
+int main(){
+	User artur;
+	artur.getName();
+	artur.setAge(10);
+	artur.getName();
+	artur.userAsk();
+	return 0;
+}
