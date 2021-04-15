eb4667afc3136be0f57dba3d06676594bc0561f3
arturkaminski <muir.e@students.rutc.ac.uk>
2021-04-15 16:19:02 +0200

    Add files via upload

---
+++ str.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a;
+    char x;
+    string b= "";
+    int i;
+
+    while( cin >> x)
+    {
+        if ( x!= '\n' )
+	{
+		i=0;	
+        	a[i] = x;
+	    	i++;
+        }
+        cout << a.length() << endl;
+	cout << a << endl;
+        break;
+    }
+    
+    return 0;
+}
