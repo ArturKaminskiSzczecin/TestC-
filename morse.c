4e042e52df1f5d6b22470b3a071164272aec1568
arturkaminski <muir.e@students.rutc.ac.uk>
2021-06-14 16:03:56 +0200

    Add files via upload

--- morse.c
+++ morse.c
@@ -7,26 +7,28 @@ const char *ascii[55] = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",  
 char *decode_morse(const char* morse_code) {
 
     //  <----  hajime!
-  int i =0;
-  int x =0;
-  char morse_copy[100];
-  char ascii_return[100];
+ int i =0;
+ char morse_copy[100];
+
+char *ascii_return = malloc (sizeof (char )* 100);
   strcpy(morse_copy, morse_code);
 
   char *segment = strtok(morse_copy, " ");
 	while(segment){
 		for(i=0; i < 55; ++i){
-			if(!strcmp(segment, morse[i]))
-			       strcpy(ascii_return[x],ascii[i]);
-				x++;
+			if(!strcmp(segment, morse[i])){
+				printf("%s", ascii[i]);
+			       strcat(ascii_return,ascii[i]);
+			}
 		}
 		segment = strtok(NULL, " ");
-		x=0;
+
 	}
- // printf("\n");
+  printf("\n");
+printf("ascii return %s\n", ascii_return); 
  return ascii_return;
 }
 int main(){
-	const char* sos = "--- ... ---";
-	decode_morse(sos);
+	const char* OSO = "--- ... ---";
+	printf("%s\n", decode_morse(OSO));
 }
