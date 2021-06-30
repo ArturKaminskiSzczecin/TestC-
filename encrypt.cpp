8ff2e123702ea73fad1c890a7c0bb5afd7d8ee5c
ArturKaminskiSzczecin <a_rudnicki@yahoo.com>
2021-06-30 10:31:54 +0200

    encrypt

--- encrypt.cpp
+++ encrypt.cpp
@@ -5,15 +5,11 @@ std::string encrypt(std::string text, int n)
 {
     int l = text.length();
     int x = 0, i=0;
-    char text1[l+1];
+    char text1[l];
     int y = l/2;
-    if(n <= 0){
-        for(int j =0; j <l; j++){
-         text1[j] = text[j];
-        }
-         return text1;
-    }
-    else{
+    std::cout << "text encrypting" << std::endl;
+    std::cout << text << std::endl;
+    if(n > 0){
             while(text[i] != '\0'){
                 //std::cout << "while started" << std::endl;
                 if ((i %2) != 0){
@@ -28,12 +24,17 @@ std::string encrypt(std::string text, int n)
             }
             i =0;
                 text1[y] = '\0';
-                n--;
                 std::cout << n << '\n';
+                n--;
                 encrypt(text1,n);
-            }
-        return text1;
+    }
+    else {
+        for(int j =0; j <l; j++){
+         text1[j] = text[j];
+        }
     
+    }
+         return text1;
 }
 std::string decrypt(std::string encryptedText, int n)
 {
@@ -42,11 +43,11 @@ std::string decrypt(std::string encryptedText, int n)
 
 int main(){
     char text[] = "This is a test!";
-    std::cout << encrypt( text, 0) << '\n';
-    std::cout << encrypt( text, 1) << '\n';
+   // std::cout << encrypt( text, 0) << '\n';
+   // std::cout << encrypt( text, 1) << '\n';
     std::cout << encrypt( text, 2) << '\n';
-    std::cout << encrypt( text, 3) << '\n';
-    std::cout << encrypt( text, 4) << '\n';
+   // std::cout << encrypt( text, 3) << '\n';
+    //std::cout << encrypt( text, 4) << '\n';
     //std::cout << encrypt( text, -1) << '\n';
 
 }
